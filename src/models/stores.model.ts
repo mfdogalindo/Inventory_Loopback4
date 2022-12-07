f4820140f47de4cf2bae4c1491aa905a0db662fa
Manuel Galindo <mfdogalindo@outlook.com>
2022-12-07 00:17:45 -0500

    wtf

---
+++ src/models/stores.model.ts
@@ -0,0 +1,73 @@
+import {Entity, hasMany, model, property} from '@loopback/repository';
+import {Products} from './products.model';
+
+@model()
+export class Stores extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  image: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  address: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  status: number;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  createdAt: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  updatedAt: string;
+
+  @hasMany(() => Products)
+  products: Products[];
+
+  constructor(data?: Partial<Stores>) {
+    super(data);
+  }
+}
+
+export interface StoresRelations {
+  // describe navigational properties here
+}
+
+export type StoresWithRelations = Stores & StoresRelations;
