81382f744e9c58c51778cb3477bfd5e01819abde
Manuel Galindo <mfdogalindo@gmail.com>
2022-12-06 21:10:31 -0500

    Initial commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './stores.controller';
+export * from './products.controller';
+export * from './categories.controller';
+export * from './users.controller';
